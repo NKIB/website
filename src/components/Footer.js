08768171a37b1a4aa7bd2f1fad39c9811ea9d559
Anton Marthedal <Antonmarthedal@gmail.com>
2023-08-10 10:15:12 +0200

    only linkin
    Ingen social media logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,8 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
         <LinkedInIcon />
       </div>
-      <p> &copy; 2022 pedrotech.com</p>
     </div>
   );
 }
